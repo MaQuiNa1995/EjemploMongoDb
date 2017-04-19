bf5ba9454a97b4229bc1c63ca484288c18e4d60e
Cristian Muñoz Asón <cmunoz@cic.es>
2017-04-19 13:23:18 +0200

    añadido de metodos de conexion con autentificacion No estan bien (Pregunte en stackoverflow)

--- src/main/java/es/cic/cmunoz/mongodbmascota/Conector.java
+++ src/main/java/es/cic/cmunoz/mongodbmascota/Conector.java
@@ -5,6 +5,10 @@
  */
 package es.cic.cmunoz.mongodbmascota;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
 import java.util.logging.Level;
@@ -20,10 +24,13 @@ public final class Conector {
 
     private static final String URLBBDD = "localhost";
     private static final int PUERTOBBDD = 27017;
+    private static final String USUARIO = "usuario";
+    private static final String CONTRASENNA = "contrasenna";
 
     public Conector() {
         super();
-        conectarBaseDatos("barracon");
+//        conectarBaseDatosConAutentificacion();
+//        crearColeccion("tropas");
         
     }
     
@@ -33,6 +40,61 @@ public final class Conector {
             MongoDatabase baseDatos = mongoClient.getDatabase(nombreBaseDatos);
             LOG.log(Level.INFO, "Conexion con la base de datos: {0} exitosa", baseDatos.getName());
             return baseDatos;
+        }
+    }
+
+    /**
+     * Método simple para la conexión de una base de datos. Hace uso de metodos
+     * deprecados
+     */
+    public void conectarBaseDatosDeprecado() {
+        try {
+
+            MongoClient mongoClient = new MongoClient(URLBBDD, PUERTOBBDD);
+
+            DB baseDatosDeprecada = mongoClient.getDB("prueba");
+
+            LOG.log(Level.INFO, "Conectando a Base De Datos: {0}...", baseDatosDeprecada.getName());
+
+            // metodo authenticate no existe
+            boolean auth = baseDatosDeprecada.authenticate(USUARIO, CONTRASENNA);
+
+            if (auth) {
+                LOG.log(Level.INFO, "Conexión Exitosa");
+            } else {
+                LOG.log(Level.INFO, "Conexión Fallida");
+            }
+
+        } catch (Exception e) {
+            LOG.log(Level.WARNING, "Excepcion Al Conectarse A La Base De Datos: {0}", e.getMessage());
+            System.exit(1);
+        }
+    }
+
+    /**
+     * Método simple para la conexión de una base de datos
+     */
+    public void conectarBaseDatosConAutentificacion() {
+
+        try {
+
+            MongoClient mongoClient = new MongoClient(URLBBDD, PUERTOBBDD);
+
+            MongoDatabase baseDatos = mongoClient.getDatabase("prueba");
+
+            LOG.log(Level.INFO, "Conectando a Base De Datos: {0}...", baseDatos.getName());
+
+            boolean auth = baseDatos.authenticate(USUARIO, CONTRASENNA);
+
+            if (auth) {
+                LOG.log(Level.INFO, "Conexión Exitosa");
+            } else {
+                LOG.log(Level.INFO, "Conexión Fallida");
+            }
+
+        } catch (Exception e) {
+            LOG.log(Level.WARNING, "Excepcion Al Conectarse A La Base De Datos: {0}", e.getMessage());
+            System.exit(1);
         }
     }
 
