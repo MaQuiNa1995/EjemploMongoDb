de6d3ca11f485d5ae660a21de39f0b4b26a1435a
Cristian Muñoz Asón <cmunoz@cic.es>
2017-04-19 13:58:17 +0200

    modificado de metodo generico para conectarse a la base de datos, quitado de deprecados , añadido de  metodo de insert y select de colecciones

--- src/main/java/es/cic/cmunoz/mongodbmascota/Conector.java
+++ src/main/java/es/cic/cmunoz/mongodbmascota/Conector.java
@@ -10,9 +10,12 @@ import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import java.util.Collection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.bson.Document;
 
 /**
  * @Autor cmunoz
@@ -24,25 +27,67 @@ public final class Conector {
 
     private static final String URLBBDD = "localhost";
     private static final int PUERTOBBDD = 27017;
-    private static final String USUARIO = "usuario";
-    private static final String CONTRASENNA = "contrasenna";
+//    private static final String USUARIO = "usuario";
+//    private static final String CONTRASENNA = "contrasenna";
+    private static final String BASEDATOS_NOMBRE = "PRUEBA";
 
+    // TODO JAVADOC
     public Conector() {
         super();
 //        conectarBaseDatosConAutentificacion();
 //        crearColeccion("tropas");
-        
     }
-    
-    public MongoDatabase conectarBaseDatos(String nombreBaseDatos){
+
+    // TODO JAVADOC
+    public MongoDatabase prepararCliente() {
         LOG.info("Intentando conectar a base de datos...");
-        try(MongoClient mongoClient = new MongoClient(URLBBDD, PUERTOBBDD)){
-            MongoDatabase baseDatos = mongoClient.getDatabase(nombreBaseDatos);
-            LOG.log(Level.INFO, "Conexion con la base de datos: {0} exitosa", baseDatos.getName());
-            return baseDatos;
+        try (MongoClient mongoClient = new MongoClient(URLBBDD, PUERTOBBDD)) {
+            MongoDatabase conexion = conexionBaseDatos(mongoClient);
+            LOG.log(Level.INFO, "Conexion con la base de datos exitosa");
+            return conexion;
         }
     }
 
+    public MongoDatabase conexionBaseDatos(MongoClient cliente) {
+        return cliente.getDatabase(BASEDATOS_NOMBRE);
+    }
+
+    // TODO JAVADOC y quitar deprecados
+    public MongoCollection<Document> buscarColeccion(String nombreColeccion) {
+
+        MongoDatabase baseDatos = prepararCliente();
+        LOG.info("Conexión Exitosa A La Base De Datos");
+
+        LOG.log(Level.INFO, "Creando La Coleccion {0} Si No Existia", nombreColeccion);
+
+        return baseDatos.getCollection(nombreColeccion);
+
+    }
+
+    // TODO JAVADOC y quitar deprecado
+    public void guardarObjetoDefault(String nombreBaseDatos, String nombreColeccion) {
+        try {
+
+            MongoDatabase baseDatos = prepararCliente();
+
+            MongoCollection<Document> coleccionGuardar = baseDatos.getCollection(nombreColeccion);
+
+            Document objetoGuardar = new Document();
+            objetoGuardar.put("nombre", "Janna");
+            objetoGuardar.put("rol", "Apoyo");
+            objetoGuardar.put("maestria", 7);
+
+            coleccionGuardar.insertOne(objetoGuardar);
+
+        } catch (Exception e) {
+            // TODO mejor explicado
+            LOG.log(Level.WARNING, "Excepcion Al Insertar Razón: {0}", e.getMessage());
+            System.exit(1);
+        }
+    }
+
+    // ----------------------- Metodos Pregunta StackOverflow -----------------------
+    // https://es.stackoverflow.com/q/63832/32964
     /**
      * Método simple para la conexión de una base de datos. Hace uso de metodos
      * deprecados
@@ -57,14 +102,13 @@ public final class Conector {
             LOG.log(Level.INFO, "Conectando a Base De Datos: {0}...", baseDatosDeprecada.getName());
 
             // metodo authenticate no existe
-            boolean auth = baseDatosDeprecada.authenticate(USUARIO, CONTRASENNA);
-
-            if (auth) {
-                LOG.log(Level.INFO, "Conexión Exitosa");
-            } else {
-                LOG.log(Level.INFO, "Conexión Fallida");
-            }
-
+//            boolean auth = baseDatosDeprecada.authenticate(USUARIO, CONTRASENNA);
+//
+//            if (auth) {
+//                LOG.log(Level.INFO, "Conexión Exitosa");
+//            } else {
+//                LOG.log(Level.INFO, "Conexión Fallida");
+//            }
         } catch (Exception e) {
             LOG.log(Level.WARNING, "Excepcion Al Conectarse A La Base De Datos: {0}", e.getMessage());
             System.exit(1);
@@ -84,18 +128,16 @@ public final class Conector {
 
             LOG.log(Level.INFO, "Conectando a Base De Datos: {0}...", baseDatos.getName());
 
-            boolean auth = baseDatos.authenticate(USUARIO, CONTRASENNA);
-
-            if (auth) {
-                LOG.log(Level.INFO, "Conexión Exitosa");
-            } else {
-                LOG.log(Level.INFO, "Conexión Fallida");
-            }
-
+//            boolean auth = baseDatos.authenticate(USUARIO, CONTRASENNA);
+//
+//            if (auth) {
+//                LOG.log(Level.INFO, "Conexión Exitosa");
+//            } else {
+//                LOG.log(Level.INFO, "Conexión Fallida");
+//            }
         } catch (Exception e) {
             LOG.log(Level.WARNING, "Excepcion Al Conectarse A La Base De Datos: {0}", e.getMessage());
             System.exit(1);
         }
     }
-
 }
